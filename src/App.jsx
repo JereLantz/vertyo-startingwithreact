b0fe9ace24b7583700a5e8bd310643e608b0b5b5
Jere Lantz <lantz.jere@gmail.com>
2025-03-03 22:19:31 +0200

    added dynamic greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Greeting from "./components/Greeting"
+
 function App() {
   return (
       <div>
       <p>Hello world</p>
+      <Greeting>Jere</Greeting>
       </div>
   )
 }
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting({children}){
+    return <h1>Hello {children}!</h1>
+}
+
+export default Greeting
