62812795eda58e831145869dd220e2386d9993a8
Jere Lantz <lantz.jere@gmail.com>
2025-03-04 15:22:35 +0200

    light/dark mode nappi tehty vähän reactimmin

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react"
 import "./ThemeToggle.css"
 
 export default function ThemeToggle(){
-    const [theme, switchTheme] = useState()
+    const [theme, switchTheme] = useState(localStorage.getItem("theme") || "light")
 
+    useEffect(() => {
+        if(theme == "dark"){
+            document.documentElement.setAttribute("data-theme", "dark")
+        }
+        else {
+            document.documentElement.removeAttribute("data-theme")
+        }
+    })
     function onClick(){
-        if(!document.documentElement.getAttribute("data-theme")){
-            switchTheme(document.documentElement.setAttribute("data-theme", "dark"))
-        }
-        else{
-            switchTheme(document.documentElement.removeAttribute("data-theme"))
-        }
+        switchTheme(theme == "dark" ? "light" : "dark")
     }
 
     return <div><button onClick={onClick}>Change theme</button></div>
